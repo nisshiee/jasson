aec05bef333c0552b953e625f29d61844dba7d96
kmizu <mizukota@gmail.com>
2011-10-29 16:12:04 +0900

    Add ScalaCodeGenerator.

---
+++ src/main/scala/org/onion_lang/jasson/ScalaCodeGenerator.scala
@@ -0,0 +1,13 @@
+package org.onion_lang.jasson
+
+/*
+ * Created by IntelliJ IDEA.
+ * User: Mizushima
+ * Date: 11/10/29
+ * Time: 11:41
+ */
+class ScalaCodeGenerator extends AnyRef with CodeGenerator[Any, Any, Any]{
+  def apply(v1: Any, v2: Any): Any = {
+
+  }
+}
