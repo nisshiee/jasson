a67025f81723729d122359c43b2417b2705756d5
kmizu <mizukota@gmail.com>
2011-10-31 23:05:11 +0900

    Partial implementation.  This wil be fixed later.

--- src/main/scala/org/onion_lang/jasson/CodeGenerator.scala
+++ src/main/scala/org/onion_lang/jasson/CodeGenerator.scala
@@ -1,9 +1,52 @@
 package org.onion_lang.jasson
 
+import scala.util.parsing.json._
 /*
  * Created by IntelliJ IDEA.
  * User: Mizushima
  * Date: 11/10/29
  * Time: 9:53
  */
-trait CodeGenerator[Schema, Settings, Output] extends ((Schema, Settings) => Output)
+trait CodeGenerator {
+  val outputDir: String
+  def generateCode(schema: JSONObject): Unit
+}
+
+object CodeGenerator {
+  private val typeMapping: Map[String, String] = Map(
+    "string" -> "String",
+    "number" -> "Double",
+    "integer" -> "Int",
+    "object" -> "Map[String, Any]",
+    "array" -> "Array[Any]"
+  )
+  private class ScalaCodeGenerator(val outputDir: String) extends CodeGenerator {
+    def generateCode(schema: JSONObject): Unit = {
+      val obj = schema.obj
+      val genClassName = obj("className")
+      obj("type") match {
+        case "string" =>
+        case "number" =>
+        case "integer" =>
+        case "object" =>
+          val className = obj("className")
+          printf("class %d(mapping: Map[String, Any]) {%n", className)
+          val properties = obj("properties").asInstanceOf[JSONObject].obj
+          for((k, tpe:Map[String, Any]) <- properties) {
+            val tpString = typeMapping(tpe("type").toString)
+            printf("def %s: %s = mapping.get(\"%s\").asInstanceOf[%s]", k, tpString, k, tpString)
+          }
+          printf("}%n")
+          for((k, tpe:JSONObject) <- properties) {
+            val obj = tpe.obj
+            val stype = typeMapping(obj("type").toString)
+            if(stype == "object") {
+              generateCode(tpe)
+            }
+          }
+        case "array" =>
+      }
+    }
+  }
+  def apply(outputDir: String): CodeGenerator = new ScalaCodeGenerator(outputDir)
+}
--- src/main/scala/org/onion_lang/jasson/JassonTool.scala
+++ src/main/scala/org/onion_lang/jasson/JassonTool.scala
@@ -1,5 +1,27 @@
+package org.onion_lang.jasson
+import io.Source
+import util.parsing.json.JSON
+import java.io.{Reader, FileReader}
+
 object JassonTool {
+  def using[T <: { def close(): Unit }, V](resource: T)(block: T => V): V = try {
+    block(resource)
+  } finally {
+    resource.close()
+  }
+  def readAll(reader: Reader): String = {
+    Iterator.continually(reader.read()).takeWhile(_ != -1).map(_.toChar).mkString("")
+  }
   def main(args: Array[String]) {
-    println("This is a JSON Scheme compiler!")
+    args match {
+      case Array("-d", dirName, inputFile) =>
+        using(new FileReader(inputFile)){r =>
+          val input = readAll(r)
+          val json = JSON.parseRaw(input)
+          CodeGenerator(inputFile).generateCode(json)
+        }
+      case _ =>
+        println("Usage: scala JassonTool -d <dir_name> input.json")
+    }
   }
 }
