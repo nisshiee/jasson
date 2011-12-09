60a6bf51e6c1bc7cf58e0efe449ccd8e40690d7f
kmizu <mizukota@gmail.com>
2011-12-10 07:43:08 +0900

    Add JaconParser.scala, which is a parser of JSON Shema Compact Syntax.

---
+++ src/main/scala/org/onion_lang/jasson/JaconParser.scala
@@ -0,0 +1,15 @@
+package org.onion_lang.jasson
+
+import util.parsing.combinator.RegexParsers
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Mizushima
+ * Date: 11/12/10
+ * Time: 7:40
+ * To change this template use File | Settings | File Templates.
+ */
+
+object JaconParser extends RegexParsers {
+
+}
