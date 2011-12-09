5d8f11e08201a8de4eccafd06efc008ea8d87e65
kmizu <mizukota@gmail.com>
2011-12-10 08:07:46 +0900

    Add JsonSchema types.

--- src/main/scala/org/onion_lang/jasson/JsonSchema.scala
+++ src/main/scala/org/onion_lang/jasson/JsonSchema.scala
@@ -8,6 +8,14 @@ package org.onion_lang.jasson
  */
 object JsonSchema {
   sealed trait SchemaEntry
-  case class CoreSchema(description: Option[String], typ: Option[String], properties: Map[String, SchemaEntry]) extends SchemaEntry
-  // TODO required AST nodes should be added later
+  sealed trait TypeSchema
+  final case class ObjectSchema(name: String, description: Option[String], properties: List[(String, SchemaEntry)]) extends SchemaEntry
+  final case class StringSchema(required: Boolean, minLength: Option[Int], maxLength: Option[Int]) extends SchemaEntry with TypeSchema
+  final case class IntegerSchema(required: Boolean, maximum: Option[Int], minimum: Option[Int]) extends SchemaEntry with TypeSchema
+  final case class NumberSchema(required: Boolean,  maximum: Option[Int], minimum: Option[Int]) extends SchemaEntry with TypeSchema
+  final case class BooleanSchema(required: Boolean) extends SchemaEntry with TypeSchema
+  final case class AnySchema(required: Boolean) extends SchemaEntry with TypeSchema
+  final case class RefSchema(required: Boolean,  ref: String) extends SchemaEntry with TypeSchema
+  final case class NullSchema(required: Boolean) extends SchemaEntry with TypeSchema
+  final case class ArraySchema(required: Boolean,  baseType: TypeSchema,  minItems: Option[Int], maxItems: Option[Int]) extends SchemaEntry with TypeSchema
 }
