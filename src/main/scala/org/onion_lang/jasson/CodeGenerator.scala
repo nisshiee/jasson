5038e1b6143de5365a5cc90dddf77ba1db08a4a7
kmizu <mizukota@gmail.com>
2011-12-10 11:04:05 +0900

    Implmented partially.

--- src/main/scala/org/onion_lang/jasson/CodeGenerator.scala
+++ src/main/scala/org/onion_lang/jasson/CodeGenerator.scala
@@ -50,7 +50,7 @@ object CodeGenerator {
       case x:RefSchema => x.ref
       case _:NullSchema => "Null"
       case ArraySchema(_, base, _, _) =>
-        "Array[" + type2String(base) + "]"
+        "List[" + type2String(base) + "]"
     }
     private def genCodeFor(property: (String,  SchemaEntry)): Unit = {
       val (name, entry) = property
@@ -59,6 +59,37 @@ object CodeGenerator {
           enter("class %s {", "}", name) {
             for(property <- properties) {
               genCodeFor(property)
+            }
+          }
+          enter("object %s {", "}", name) {
+            enter("def loadFrom(json: JSONObject): %s = {", "}", name) {
+              pn("val newObject = new %s()", name)
+              for((name, entry) <- properties) entry match {
+                case StringSchema(req, min, max) =>
+                  pn("newObject.%s = json.obj(\"%s\").asInstanceOf[String]", name, name)
+                case IntegerSchema(req, min, max) =>
+                  pn("newObject.%s = json.obj(\"%s\").asInstanceOf[String].toInt", name, name)
+                case NumberSchema(req, min, max) =>
+                  pn("newObject.%s = json.obj(\"%s\").asInstanceOf[String].toDouble", name, name)
+                case BooleanSchema(req) =>
+                  pn("newObject.%s = json.obj(\"%s\").asInstanceOf[String].toBoolean", name, name)
+                case AnySchema(req) =>
+                  pn("newObject.%s = json.obj(\"%s\").asInstanceOf[Any]", name, name)
+                case RefSchema(req, ref) =>
+                  pn("newObject.%s = %s.loadFrom(json.obj(\"%s\").asInstanceOf[JSONObject])", name, ref, name)
+                case NullSchema(req) =>
+                  pn("newObject.%s = json.obj(\"%s\").asInstanceOf[Null]", name, name)
+                case ArraySchema(req, base, minItems, maxItems) =>
+                  pn("val newList = json.obj(\"%s\").asInstanceOf[JSONArray].list", name)
+                  enter("for(e <- newList) {", "}") {
+                    pn("newObject.%s = e :: newObject.%s", name, name)
+                  }
+                  pn("newObject.%s = newObject.%s.reverse", name, name)
+              }
+              pn("newObject")
+            }
+            enter("def loadFrom(jsonStr: String): %s = {", "}", name) {
+              pn("loadFrom(JSON.parseRaw(jsonStr).get.asInstanceOf[JSONObject])")
             }
           }
         case StringSchema(req, min, max) =>
@@ -76,11 +107,12 @@ object CodeGenerator {
         case NullSchema(req) =>
           pn("var %s : %s = null", name, "Null")
         case ArraySchema(req, base, minItems, maxItems) =>
-          pn("var %s : %s = null", name, type2String(base))
+          pn("var %s : %s = Nil", name, type2String(base))
       }
     }
     def generateCode(unit: SchemaUnit): Unit = {
       pn(unit.preamble)
+      pn("import scala.util.parsing.json._")
       genCodeFor((unit.name,  unit.schema))
     }
   }
